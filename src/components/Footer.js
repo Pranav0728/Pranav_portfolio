f57213111ecdb6ea1360dc9bea2ac9c0b552c16b
Pranav Molawade <112539982+Pranav0728@users.noreply.github.com>
2024-06-25 11:30:12 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/soumyajit4419"
+                href="https://www.instagram.com/pranav_2_8/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
